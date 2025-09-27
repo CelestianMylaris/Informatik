e238f62bbe1905ef34d62ff8a058656eab4ca79c
Informatik <inf@famila-22.1-thinkpad590>
2025-09-27 21:02:30 +0200

    changed Geometrie

--- Geometrie/build.sbt
+++ Geometrie/build.sbt
@@ -1,8 +1,12 @@
-ThisBuild / version := "0.1.0-SNAPSHOT"
+val scala3Version = "3.7.3"
 
-ThisBuild / scalaVersion := "3.3.6"
-
-lazy val root = (project in file("."))
+lazy val root = project
+  .in(file("."))
   .settings(
-    name := "Geometrie"
+    name := "Geometrie",
+    version := "0.1.0-SNAPSHOT",
+
+    scalaVersion := scala3Version,
+
+    libraryDependencies += "org.scalameta" %% "munit" % "1.0.0" % Test
   )
--- Geometrie/src/main/scala/Geo.scala
+++ Geometrie/src/main/scala/Geo.scala
@@ -7,15 +7,11 @@
 // ein Punkt in der Form (3.1 | 5) als String dargestellt wird.
 // Ergänzen Sie eine Methode abstandZu(p: Punkt), die den Abstand von
 // this zu p berechnet und zurückgibt.
-case class Punkt(x: Double, y:Double):
+case class Punkt(x: Double, y: Double):
   override def toString: String = s"$x / $y"
-  def abstandZu(p: Punkt): Double = Math.sqrt((p.x - x)*(p.x - x) + (p.y - y)*(p.y - y))
+  def abstandZu(p: Punkt): Double =
+    Math.sqrt((p.x - x) * (p.x - x) + (p.y - y) * (p.y - y))
 
-val p1 = Punkt(2,3)
-p1.toString
-val p2 = Punkt(4,6)
-p1.abstandZu(p2)
-p1 abstandZu p2
 // Begründen Sie, dass bei einer mutablen Klasse die Entscheidung zwischen
 // private- und public-Attribute wichtiger ist als bei einer immutablen Klasse.
 
@@ -28,9 +24,8 @@ p1 abstandZu p2
 
 class Gerade(p1: Punkt, p2: Punkt):
   override def toString: String = s"Gerade durch ($p1 ) und ($p2 )"
-
-val g1 = Gerade(p1, p2)
-g1.toString
+  val g1 = Gerade(p1, p2)
+  g1.toString
 
 // Definieren Sie eine immutable Klasse Strecke, die durch zwei Punkte
 // definiert wird. Überschreiben Sie die toString-Methode so, dass die
@@ -40,21 +35,19 @@ g1.toString
 
 class Strecke(p1: Punkt, p2: Punkt):
   override def toString: String = s"Strecke von ($p1 ) nach ($p2 )"
-  val mittelpunkt: Punkt = Punkt((p1.x + p2.x)/2, (p1.y + p2.y) / 2)
+  val mittelpunkt: Punkt = Punkt((p1.x + p2.x) / 2, (p1.y + p2.y) / 2)
   def laenge: Double = p1.abstandZu(p2)
   val steigung: Double = (p2.y - p1.y) / (p2.x - p1.x)
   val normalenSteigung: Double = -1 / steigung
-  def mittelsenkrechte: Gerade = Gerade(mittelpunkt, Punkt(mittelpunkt.x + 1,mittelpunkt.y + normalenSteigung ) )
+  def mittelsenkrechte: Gerade = Gerade(
+    mittelpunkt,
+    Punkt(mittelpunkt.x + 1, mittelpunkt.y + normalenSteigung)
+  )
 
 // Definieren Sie eine immutable Klasse Kreis mit Mittelpunkt und Radius.
-case class Kreis(r: Int, m: Punkt)
-
-
-
-
-val k1 = Kreis.apply(10,Punkt(0,0))
-val k2 = k1.copy(m=Punkt(1,1))
-
+case class Kreis(r: Int, m: Punkt):
+  val k1 = Kreis.apply(10, Punkt(0, 0))
+  val k2 = k1.copy(m = Punkt(1, 1))
 
 // Neuer Stoff: Nebenkonstruktoren ("secondary constructors")
 // und Factory-Methoden.
@@ -66,4 +59,3 @@ val k2 = k1.copy(m=Punkt(1,1))
 // Neuer Stoff: Vorgabeparameter, copy-Methode für immutable Klassen
 // Instanzen immutabler Klassen kann man nicht verändern. Es ist aber hilreich,
 // veränderte neue Instanzen erzeugen zu können. Dazu dient die copy-Methode.
-// Sie hat Parameter
